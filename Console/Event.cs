53acdc5e2d65c29185c82544bf51fb3716cfe420
= <martyofmca@gmail.coms>
2022-12-20 21:08:03 +0000

    Create Event model

---
+++ Console/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace Console
+{
+    internal class Event
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public Shared.Priority Priority { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateHappening { get; set; }
+    }
+}
