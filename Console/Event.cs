0088ddf3d6a05a1be4142a1a76da787ba0d496db
= <martyofmca@gmail.coms>
2022-12-22 00:55:55 +0000

    Feature to create and add an event
    Implemented the feature where an event can be created to store all the details for the event and also to store the event created

--- Console/Event.cs
+++ Console/Event.cs
@@ -2,10 +2,25 @@
 {
     internal class Event
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string? Description { get; set; }
         public Shared.Priority Priority { get; set; }
         public DateTime DateAdded { get; set; }
         public DateTime DateHappening { get; set; }
+
+        public Event(string name, string? description, Shared.Priority priority, DateTime dateHappening)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Priority = priority;
+            this.DateHappening= dateHappening;
+        }
+
+        public DateTime AddEvent()
+        {
+
+            DateAdded = DateTime.Now;
+            return DateAdded;
+        }
     }
 }
